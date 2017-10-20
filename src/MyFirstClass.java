fdfd3e1c62ac8820e39d56300e37d25bb34f0961
caunovas <nice@cau.ac.kr>
2017-10-20 11:06:26 +0900

    java업로드용

---
+++ src/MyFirstClass.java
@@ -0,0 +1,11 @@
+public class MyFirstClass {
+	public static void main(String[] args) {
+		System.out.println(858373);
+		
+			int i;
+			short s=1;
+			byte b=1;
+			long l=1;
+			float f=1.1F;
+	}
+}
